14dcdc8b0d2896ddf002da9921002230001ede3c
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-03 19:49:51 +0300

    Added getters and setter

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ class Teacher:
         self.__name = name
         self.__education = education
         self.__experience = experience
+
+    def get_name(self):
+        return self.__name
+
+    def get_education(self):
+        return self.__education
+
+    def get_experience(self):
+        return self.__experience
+
+    def set_experience(self, experience):
+        self.__experience = experience
+        return True
